1d63341676897acd3126a39ccbdcd7b0781602d6
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2022-11-20 18:32:53 +0100

    update @angular/cli

--- src/app/sidemenu/sidemenu.component.ts
+++ src/app/sidemenu/sidemenu.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
-import { RouterLinkWithHref, RouterModule } from '@angular/router';
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'eternal-sidemenu',
   templateUrl: './sidemenu.component.html',
   styleUrls: ['./sidemenu.component.scss'],
   standalone: true,
-  imports: [CommonModule, MatButtonModule, RouterLinkWithHref],
+  imports: [CommonModule, MatButtonModule, RouterLink],
 })
 export class SidemenuComponent {}
