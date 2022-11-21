39269e930ced0ec42ac0eef71936b0621a0a8363
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2022-11-21 21:54:17 +0100

    add ngOptimizedImage

--- src/app/holidays.component.ts
+++ src/app/holidays.component.ts
@@ -1,20 +1,22 @@
 import {Component} from '@angular/core';
 import {holidays} from './holidays.data';
-import {NgForOf} from "@angular/common";
+import {NgForOf, NgOptimizedImage, provideCloudinaryLoader} from "@angular/common";
 
 @Component({
   template: `<h2>Holidays</h2>
 
-  <div class="relative my-4" *ngFor="let holiday of holidays">
-    <img [src]="'/assets/' + holiday.imageUrl" />
+  <div class="relative my-4" *ngFor="let holiday of holidays; first as isFirst">
+    <img [ngSrc]="holiday.imageUrl" width="1920" height="1080" sizes="100vw" [priority]="isFirst" />
     <h1 class="absolute text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 centered drop-shadow-2xl">{{holiday.title}}</h1>
   </div>`,
   imports: [
-    NgForOf
+    NgForOf,
+    NgOptimizedImage
   ],
   styles: [ 'h1 {font-size: min(15vw, 180px); text-shadow: #000 1px 0 15px;}'],
   standalone: true,
+  providers: [provideCloudinaryLoader('https://res.cloudinary.com/dhidasbqj')]
 })
 export class HolidaysComponent {
-  holidays = holidays;
+  holidays = holidays.map(holiday => ({...holiday, imageUrl: `/angular-15/${holiday.imageUrl}`}))
 }
